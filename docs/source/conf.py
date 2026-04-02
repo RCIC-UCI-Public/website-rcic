7e2286110d91ca37f55380277c1977713ae4f680
npw <npw@uci.edu>
2026-04-02 13:58:00 -0700

    add overwrite code for mailto elements. This helps to avoid accessibility checkers duplicate link warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,3 +100,29 @@ html_theme_options = {
 }
 # enable figure auto numbering
 numfig = True
+
+# this code is for overwriting mailto: directives.
+# sphinx creates <span> tags  in mailto links which confuse
+# accessibility checkers into flagging  such links as duplicate
+from docutils import nodes
+from sphinx.writers.html import HTML5Translator
+
+class CleanEmailTranslator(HTML5Translator):
+    def visit_reference(self, node):
+        # If it's a mailto link, prevent the theme from splitting it
+        if node.get('refuri', '').startswith('mailto:'):
+            atts = {'class': 'reference external', 'href': node['refuri']}
+            self.body.append(self.starttag(node, 'a', '', **atts))
+        else:
+            super().visit_reference(node)
+
+    def depart_reference(self, node):
+        if node.get('refuri', '').startswith('mailto:'):
+            self.body.append('</a>')
+        else:
+            super().depart_reference(node)
+
+def setup(app):
+    # This forces Sphinx to use our custom 'clean' translator
+    app.set_translator('html', CleanEmailTranslator)
+
